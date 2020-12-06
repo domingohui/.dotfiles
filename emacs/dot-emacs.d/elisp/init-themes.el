7e6cef010f74a84e681ca79b10e63b415666ab03
Domingo Hui <domingohui@gmail.com>
2020-12-06 15:06:14 -0800

    Define a way to manage and change custom themes

--- emacs/dot-emacs.d/elisp/init-themes.el
+++ emacs/dot-emacs.d/elisp/init-themes.el
@@ -5,4 +5,29 @@
   )
 
 
+;; Define themes to use
+;; Themes should be trusted; no confirm prompt will be shown.
+(defvar enabled-themes
+  (list 'sanityinc-tomorrow-day
+	'sanityinc-tomorrow-night
+	'spacemacs-light
+	)
+  )
+
+;; Functions to change between themes
+(defun choose-theme (chosen-theme)
+  "Prompt for a theme selection (CHOSEN-THEME) and load it."
+  (interactive
+   (let* ((choices-alist (zip (string-to-list "abcdefghijklmnopqrstuvwxyz") enabled-themes))
+	  (prettify-choice (lambda (x) (format "%c: %s" (car x) (cdr x))))
+	  (prompt-msg (mapconcat prettify-choice choices-alist "\n"))
+	  (choice-char (read-char-choice prompt-msg (mapcar 'car choices-alist))))
+     (cdr (assoc choice-char choices-alist))
+     ))
+  ;; Disable all custom themes (i.e. load default Emacs theme)
+  ;; This resets anything changed by the current theme.
+  (custom-set-variables '(custom-enabled-themes nil))
+  (load-theme chosen-theme t)
+  )
+
 (provide 'init-themes)
